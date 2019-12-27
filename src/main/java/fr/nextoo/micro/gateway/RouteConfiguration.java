fdee108f49d56762b74fe310b9bfe90779801cdd
Kevin Messiaen <kmessiaen@nextoo.fr>
2019-12-27 11:32:24 +0100

    route config

---
+++ src/main/java/fr/nextoo/micro/gateway/RouteConfiguration.java
@@ -0,0 +1,19 @@
+package fr.nextoo.micro.gateway;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RouteConfiguration {
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(r -> r.path("/user/**").uri("lb://USER-SERVICE"))
+                .route(r -> r.path("/product/**").uri("lb://PRODUCT-SERVICE"))
+                .route(r -> r.path("/order/**").uri("lb://ORDER-SERVICE"))
+                .build();
+    }
+}
